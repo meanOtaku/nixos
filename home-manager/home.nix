ce2fbc6ec2d4036787d6c57c1e4c0b270271974d
Vaibhav Bisht <56401767+meanOtaku@users.noreply.github.com>
2023-10-07 14:39:52 +0530

    hyprland

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,7 +5,6 @@
 	./apps/micro.nix
   ./apps/zsh.nix
   ./apps/fzf.nix  
-  ./apps/hyprland.nix  
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,4 +139,48 @@
   system.stateVersion = "23.05"; # Did you read the comment?
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Enabling hyprlnd on NixOS
+  programs.hyprland = {
+    enable = true;
+    nvidiaPatches = true;
+    xwayland.enable = true;
+    environment.systemPackages = [
+        # waybar
+        (pkgs.waybar.overrideAttrs (oldAttrs: {
+            mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+          })
+        )
+    ]
+  };
+
+  environment.sessionVariables = {
+    # If your cursor becomes invisible
+    WLR_NO_HARDWARE_CURSORS = "1";
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware = {
+      # Opengl
+      opengl.enable = true;
+
+      # Most wayland compositors need this
+      nvidia.modesetting.enable = true;
+  };
+
+  # XDG portal
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
 }
