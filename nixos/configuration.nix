ebc02bc806a149dbc792d6752993fc150cfa9f8e
Vaibhav Bisht <56401767+meanOtaku@users.noreply.github.com>
2023-10-07 14:42:15 +0530

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -69,7 +69,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -172,15 +172,5 @@
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
 
 }
