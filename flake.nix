32b726bfc6d44ee88d0b79747c58861090925a06
Vaibhav Bisht <56401767+meanOtaku@users.noreply.github.com>
2023-10-08 15:26:37 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, home-manager, ... } @ inputs:
+  outputs = { nixpkgs, home-manager, hyprland, ... } @ inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
