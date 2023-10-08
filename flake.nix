ee6781bfdc248bb47dbe04b43f4dbf4282ae7aad
Vaibhav Bisht <56401767+meanOtaku@users.noreply.github.com>
2023-10-08 11:29:27 +0530

    hyprland

--- flake.nix
+++ flake.nix
@@ -3,18 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; }; # this is the important part
           modules = [
             ./nixos/configuration.nix
           ];
