df45a397f84797ebcf5bb7d53a0f98fc6db9bd5d
Vaibhav Bisht <56401767+meanOtaku@users.noreply.github.com>
2023-10-14 20:33:42 +0530

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,30 @@
 {
-  description = "Your new nix config";
+  description = "NixOS configuration with Home Manager";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          modules = [
-            ./nixos/configuration.nix
-          ];
-        };
-      };
-      homeConfigurations = {
-        vaibhav = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            ./home-manager/home.nix
-          ];
-        };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.vaibhav = import ./home-manager/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
       };
     };
+  };
 }
